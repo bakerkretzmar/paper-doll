544fac7b5449c28ec0199906fabc580dcb2ac7c8
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2020-07-15 20:03:04 -0400

    Layer all clothes correctly

--- README.md
+++ README.md
@@ -3,8 +3,3 @@ This repo contains the code for the paper doll dress-up game at [oddthing.ca/t-b
 [Original proof-of-concept on CodePen.](https://codepen.io/bakerkretzmar/pen/YzyvEKX)
 
 All image files and artwork copyright 2020 Anastasia Popova.
-
-**Todo**
-
-- layer everything correctly (e.g. socks always underneath)
-    - allow clicking to bring to top
--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-const clothes = ['black-jeans', 'black-heels', 'black-shorts', 'black-sweater', 'black-tights', 'blue-jeans', 'bralette', 'button-up', 'converse', 'cowboy-boots', 'fishnets', 'green-cowboy', 'green-pants', 'green-socks', 'leather-jacket', 'orange-heels', 'orange-sweater', 'orange-tights', 'pink-heels', 'pink-jumpsuit', 'pink-socks', 'plaid-skirt', 'purple-slacks', 'purple-tights', 'quilt-skirt', 'rain-coat', 'red-cowboy', 'red-dress', 'red-gloves', 'torn-tank', 'white-socks', 'yellow-blouse', 'yellow-socks'];
+const clothes = {
+    accessories: {
+        z: 40,
+        items: ['red-gloves'],
+    },
+    bottoms: {
+        z: 50,
+        items: ['black-jeans', 'black-shorts', 'blue-jeans', 'green-pants', 'plaid-skirt', 'purple-slacks', 'quilt-skirt'],
+    },
+    hats: {
+        z: 90,
+        items: ['green-cowboy', 'red-cowboy'],
+    },
+    outerwear: {
+        z: 80,
+        items: ['leather-jacket', 'rain-coat'],
+    },
+    shoes: {
+        z: 30,
+        items: ['black-heels', 'converse', 'cowboy-boots', 'orange-heels', 'pink-heels'],
+    },
+    socks: {
+        z: 20,
+        items: ['green-socks', 'pink-socks', 'white-socks', 'yellow-socks',],
+    },
+    sweaters: {
+        z: 70,
+        items: ['black-sweater', 'orange-sweater'],
+    },
+    tops: {
+        z: 60,
+        items: ['button-up', 'pink-jumpsuit', 'red-dress', 'torn-tank', 'yellow-blouse'],
+    },
+    underwear: {
+        z: 10,
+        items: ['black-tights', 'bralette', 'fishnets', 'orange-tights', 'purple-tights'],
+    },
+};
 
-clothes.forEach((item, i) => {
-    let el = document.createElement('div');
-    el.dataset.clothing = item;
+let cell = 0;
 
-    let col = i % 6;
-    let row = (i - col) / 6;
-    el.style.position = 'absolute';
-    el.style.left = `${2 + 8 * col}rem`;
+for (category in clothes) {
+    clothes[category].items.forEach((item) => {
+        let el = document.createElement('div');
+        el.dataset.clothing = item;
+        el.style.zIndex = clothes[category].z;
+
+        let col = cell % 6;
+        let row = (cell - col) / 6;
+        el.style.position = 'absolute';
+        el.style.left = `${2 + 8 * col}rem`;
     el.style.top = `${2 + 10 * row}rem`;
 
     let img = document.createElement('img');
@@ -19,9 +60,11 @@ clothes.forEach((item, i) => {
     img.classList.add('select-none')
     img.src= `./img/${item}.png`;
 
-    el.appendChild(img);
-    document.body.appendChild(el);
-});
+        el.appendChild(img);
+        document.body.appendChild(el);
+        cell++;
+    });
+}
 
 const dollArea = document.getElementById('doll-area');
 
